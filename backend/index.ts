31d1c8c7c4e8716aa40556f177f54d36a4c49db9
AaronGr <agrav12825@gmail.com>
2025-08-12 11:11:52 -0400

    express server get tasks endpoint working for local

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,44 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import pkg from 'pg';
+
+dotenv.config();
+
+const { Pool } = pkg;
+
 
 const app = express();
 const port = 3000
 
+app.use(cors());
 app.use(express.json());
 
-app.get('/api/hello', (req, res) => res.json({ message: 'Hello World' }));
+// Postgres connection
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
 
-app.listen(port, () => {
-  console.log(`task_manager app is listening on port ${port}`)
-})
+// Test route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// GET all tasks
+app.get('/tasks', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM tasks ORDER BY due_date ASC');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
